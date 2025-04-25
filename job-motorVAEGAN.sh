7fa15f5d955d78b51faa7c75f7fd4e5665db618c
Brenden Eum <beum@caltech.edu>
2025-04-25 16:02:25 -0400

    smVG test 6

--- job-motorVAEGAN.sh
+++ job-motorVAEGAN.sh
@@ -23,17 +23,17 @@ source env/bin/activate
 python motorVAEGAN-withSupervision.py \
     --data_dir data/evox_256x256_1-4 --img_size 256 \
     --latent_dim 128 \
-    --max_kld_weight 0.75 \
-    --min_tc_weight 1.0 --max_tc_weight 2.0 \
+    --max_kld_weight 1.0 \
+    --min_tc_weight 1.0 --max_tc_weight 1.0 \
     --adv_weight 1.0 --recon_sample_weight 0.5 \
-    --cls_weight 1.0 --cls_latent_dim 16 \
+    --cls_weight 0.5 --cls_latent_dim 6 \
     --label_file data/labels_evox_256x256_1-4.csv --label_cols Year,Brand,Body,Door \
     --learning_rate 0.0001 --epochs 200 --batch_size 128 \
     --train --reconstructions --extract_latent --sample \
     --classification_accuracy --visualize_latent_class --feature_attribution \
     --traversals 2021_Toyota_CamryHybrid_XLE_sedan_4Door_2.png \
     --interpolate 2007_Toyota_PriusHybrid_nan_hatchback_5Door_3.png 2025_Polestar_Polestar4_LongRangeDualMotor_SUV_4Door_1.png \
-    --track_reconstruction 2025_Toyota_bZ4X_Nightshade_SUV_4Door_2.png
+    --track_reconstruction 2014_Nissan_Versa_SL_sedan_4Door_1.png
 
 # Notes
 # 200 epochs takes approx 13 hrs.
