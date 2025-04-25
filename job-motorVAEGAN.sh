e201a2c3b4d091085afd50ef31d8263f4a43e9b3
Brenden Eum <beum@caltech.edu>
2025-04-25 16:06:15 -0400

    kld 1.5 clsw .5

--- job-motorVAEGAN.sh
+++ job-motorVAEGAN.sh
@@ -26,7 +26,7 @@ python motorVAEGAN-withSupervision.py \
     --max_kld_weight 1.5 \
     --min_tc_weight 1.0 --max_tc_weight 1.0 \
     --adv_weight 1.0 --recon_sample_weight 0.5 \
-    --cls_weight 1.0 --cls_latent_dim 6 \
+    --cls_weight 0.5 --cls_latent_dim 6 \
     --label_file data/labels_evox_256x256_1-4.csv --label_cols Year,Brand,Body,Door \
     --learning_rate 0.0001 --epochs 200 --batch_size 128 \
     --train --reconstructions --extract_latent --sample \
