df686765ea03dd2d555080595a69b8bfb0faf12c
Brenden Eum <beum@caltech.edu>
2025-04-26 15:12:29 -0400

    old kld weight

--- job-motorVAEGAN.sh
+++ job-motorVAEGAN.sh
@@ -20,11 +20,11 @@ module load scipy-stack/2021a
 source env/bin/activate
 
 # Train
-python motorVAEGAN-withSupervision.py \
+python motorVAEGAN-withSupervisionOldKLD.py \
     --data_dir data/evox_256x256_1-4 --img_size 256 \
     --latent_dim 128 \
-    --max_kld_weight 3.0 \
-    --min_tc_weight 1.0 --max_tc_weight 1.0 --dwkl_weight 4.0 \
+    --max_kld_weight 1.0 \
+    --min_tc_weight 1.0 --max_tc_weight 1.0 --dwkl_weight 1.0 \
     --adv_weight 0.5 --recon_sample_weight 0.5 \
     --cls_weight 0.5 --cls_latent_dim 16 \
     --label_file data/labels_evox_256x256_1-4.csv --label_cols Year,Brand,Body,Door \
