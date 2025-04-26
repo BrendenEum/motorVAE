903121b59645f4f3fb56d6818b7a3d047eee825b
Brenden Eum <beum@caltech.edu>
2025-04-26 12:50:46 -0400

    dwkl 4.0

--- job-motorVAEGAN.sh
+++ job-motorVAEGAN.sh
@@ -24,7 +24,7 @@ python motorVAEGAN-withSupervision.py \
     --data_dir data/evox_256x256_1-4 --img_size 256 \
     --latent_dim 128 \
     --max_kld_weight 3.0 \
-    --min_tc_weight 1.0 --max_tc_weight 1.0 \
+    --min_tc_weight 1.0 --max_tc_weight 1.0 --dwkl_weight 4.0 \
     --adv_weight 0.5 --recon_sample_weight 0.5 \
     --cls_weight 0.5 --cls_latent_dim 16 \
     --label_file data/labels_evox_256x256_1-4.csv --label_cols Year,Brand,Body,Door \
