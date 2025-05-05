9f6ece7934507d5808071e98a6e8a0c926e1257a
Brenden Eum <beum@caltech.edu>
2025-05-05 18:40:26 -0400

    param test

--- motorVAE.py
+++ motorVAE.py
@@ -33,15 +33,15 @@ BETA2 = 0.999 # Default for Adam optimizer
 TRAIN_PROPORTION = 0.98 # Proportion of data to use for training. Validation is 1-p(train).
 
 # Create weights for different loss components
-RECON_WEIGHT = 100.0
-PERCEPTUAL_WEIGHT = 5.0
+RECON_WEIGHT = 75.0
+PERCEPTUAL_WEIGHT = 2.5
 GAN_WEIGHT = 0.2
 KLD_WEIGHT_START = 0.0001 # KLD Scheduler
-KLD_WEIGHT_END = 0.75
-TC_WEIGHT = 0.002  # Total Correlation weight
-MI_WEIGHT = 0.1  # Mutual Information weight
-DKLD_WEIGHT = 0.00002  # Dimension-wise KL Divergence weight
-CLS_WEIGHT = 0.20  # Classifier weight
+KLD_WEIGHT_END = 0.3
+TC_WEIGHT = 0.02  # Total Correlation weight
+MI_WEIGHT = 0.2  # Mutual Information weight
+DKLD_WEIGHT = 0.00001  # Dimension-wise KL Divergence weight
+CLS_WEIGHT = 0.25  # Classifier weight
 
 DISC_WEIGHT = 1.0 # Separate loss function, so this doesn't matter. 
 
