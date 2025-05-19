c2c92426e9c481fe8ca2c88b13412bb6f98c20b0
Brenden Eum <beum@caltech.edu>
2025-05-19 15:45:26 -0400

    param

--- motorVAE.py
+++ motorVAE.py
@@ -24,8 +24,8 @@ print(f"Using device: {device}")
 
 # Define hyperparameters
 IMAGE_SIZE = 256
-BATCH_SIZE = 71 #36
-EPOCHS = 140
+BATCH_SIZE = 71
+EPOCHS = 100
 LATENT_DIM = 128
 LEARNING_RATE = 0.0001
 BETA1 = 0.5 # AI recommended for GAN training
@@ -35,9 +35,9 @@ TRAIN_PROPORTION = 0.98 # Proportion of data to use for training. Validation is 
 # Create weights for different loss components
 RECON_WEIGHT = 100.0
 PERCEPTUAL_WEIGHT = 5.0
-GAN_WEIGHT = 0.4
+GAN_WEIGHT = 0.2
 KLD_WEIGHT_START = 0.0001 # KLD Scheduler
-KLD_WEIGHT_END = 0.4
+KLD_WEIGHT_END = 0.1
 TC_WEIGHT = 0.002  # Total Correlation weight
 MI_WEIGHT = 0.1  # Mutual Information weight
 DKLD_WEIGHT = 0.00002  # Dimension-wise KL Divergence weight
