e40c43d0916d6ec6df1449226ecdb6c3d8036325
Brenden Eum <beum@caltech.edu>
2025-06-25 14:50:36 -0700

    larger batch

--- motorVAE.py
+++ motorVAE.py
@@ -25,8 +25,8 @@ print(f"Using device: {device}")
 
 # Define hyperparameters
 IMAGE_SIZE = 256
-BATCH_SIZE = 71
-EPOCHS = 150
+BATCH_SIZE = 100
+EPOCHS = 100
 LATENT_DIM = 128
 LEARNING_RATE = 0.0001
 BETA1 = 0.5 # AI recommended for GAN training
@@ -34,12 +34,12 @@ BETA2 = 0.999 # Default for Adam optimizer
 TRAIN_PROPORTION = 0.98 # Proportion of data to use for training. Validation is 1-p(train).
 
 # Create weights for different loss components
-RECON_WEIGHT = 100.0
+RECON_WEIGHT = 125.0
 PERCEPTUAL_WEIGHT = 5.0
 GAN_WEIGHT = 0.2
 KLD_WEIGHT_START = 0.00001 # KLD Scheduler
-KLD_WEIGHT_END = 0.08
-TC_WEIGHT = 0.2  # Total Correlation weight
+KLD_WEIGHT_END = 0.1
+TC_WEIGHT = 0.1  # Total Correlation weight
 MI_WEIGHT = 0.1  # Mutual Information weight
 DKLD_WEIGHT = 0.00002  # Dimension-wise KL Divergence weight
 CLS_WEIGHT = 0.2  # Classifier weight
