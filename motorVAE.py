3f524390ed45264ab1c0706137f245adff471832
Brenden Eum <beum@caltech.edu>
2025-07-05 18:19:59 -0700

    typo

--- motorVAE.py
+++ motorVAE.py
@@ -39,10 +39,10 @@ PERCEPTUAL_WEIGHT = 5.0
 GAN_WEIGHT = 0.2
 KLD_WARMUP = 5 # number of epochs before initiating linear KLD weight increase
 KLD_WEIGHT_START = 0.00001 # KLD Scheduler
-KLD_WEIGHT_END = 0.06 # Final KLD weight
+KLD_WEIGHT_END = 0.6 # Final KLD weight
 TC_WEIGHT = 0.1  # Total Correlation weight
 MI_WEIGHT = 0.05  # Mutual Information weight
-DKLD_WEIGHT = 0.0005  # Dimension-wise KL Divergence weight
+DKLD_WEIGHT = 0.00055  # Dimension-wise KL Divergence weight
 CLS_WEIGHT = 0.3  # Classifier weight
 
 DISC_WEIGHT = 1.0 # Separate loss function, so this doesn't matter. 
