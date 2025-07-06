10c8b481c2e7a51717ce11c072fabb10eb72db98
Brenden Eum <beum@caltech.edu>
2025-07-05 19:19:03 -0700

    stable train

--- motorVAE.py
+++ motorVAE.py
@@ -41,8 +41,8 @@ KLD_WARMUP = 5 # number of epochs before initiating linear KLD weight increase
 KLD_WEIGHT_START = 0.00001 # KLD Scheduler
 KLD_WEIGHT_END = 0.6 # Final KLD weight
 TC_WEIGHT = 0.1  # Total Correlation weight
-MI_WEIGHT = 0.05  # Mutual Information weight
-DKLD_WEIGHT = 0.00055  # Dimension-wise KL Divergence weight
+MI_WEIGHT = 0.04  # Mutual Information weight
+DKLD_WEIGHT = 0.0003  # Dimension-wise KL Divergence weight
 CLS_WEIGHT = 0.3  # Classifier weight
 
 DISC_WEIGHT = 1.0 # Separate loss function, so this doesn't matter. 
